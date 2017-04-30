607f9968762e9ecabc05d00bdbe245791b2d21ec
Andrew Darby <andrewbdarby@gmail.com>
2017-04-30 10:49:22 -0700

    minor revision

--- scripts/landing.js
+++ scripts/landing.js
@@ -7,7 +7,8 @@
          points[index].style.msTransform = "scaleX(1) translateY(0)";
          points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 	 }	 
+	 
 		 for (var i = 0; i < points.length; i++) {
-			revealPoint[i];
+			revealPoint(i);
    }	
  };
